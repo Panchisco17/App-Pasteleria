f981488e488ae622c2148f1c28c78633cdcad24f
Francisco <fra.olivaresp@duocuc.cl>
2025-11-28 23:55:50 -0300

    configuracion test

--- app/src/main/java/com/example/app_pasteleria/MainActivity.kt
+++ app/src/main/java/com/example/app_pasteleria/MainActivity.kt
@@ -7,15 +7,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.app_pasteleria.navigation.AppNav
+import androidx.core.view.WindowCompat
 import com.example.app_pasteleria.ui.theme.AppPasteleriaTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.app_pasteleria.ui.screens.PostScreen
+import com.example.app_pasteleria.ui.theme.ApiRestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Permite que la app dibuje contenido debajo de las barras del sistema
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+// Aquí inicia Jetpack Compose
         setContent {
-            AppNav()
+            ApiRestTheme {
+                val postViewModel: com.example.app_pasteleria.viewmodel.PostViewModel = viewModel()
+                PostScreen(viewModel = postViewModel)
+            }
         }
     }
 }
